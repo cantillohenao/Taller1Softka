899b0461e1a3558fa3ef3d8963ca91056062a56a
Juan Camilo Cantillo Henao <73416660+cantillohenao@users.noreply.github.com>
2022-02-03 10:08:39 -0500

    first commit

---
+++ taller1/src/main/java/com/softka/domain/BankAccount.java
@@ -0,0 +1,5 @@
+package com.softka.domain;
+
+public class BankAccount {
+    
+}
---
+++ taller1/src/main/java/com/softka/domain/Person.java
@@ -0,0 +1,11 @@
+package com.softka.domain;
+
+import java.util.Date;
+
+public class Person {
+    private String name;
+    public String lastName1;
+    public String lastName2;
+    protected Date dateBirth;
+    protected float height;
+}
